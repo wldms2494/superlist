83308ff5b48798e6897650cbe770c6ab83833998
wldms2494 <wldms2494@gmail.com>
2022-04-11 09:33:33 +0900

    final steps similarBoats

---
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -0,0 +1,55 @@
+import { api, LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
+
+const BOAT_OBJECT = 'Boat__c';
+export default class SimilarBoats extends NavigationMixin(LightningElement) {
+    
+    currentBoat;
+    relatedBoats;
+    boatId;
+    error;
+
+    @api
+    get recordId(){
+        return this.boatId;
+    }
+
+    set recordId(value){
+        this.setAttribute('boatId', value);
+        this.boatId = value;
+    }
+
+
+    @api
+    similarBy;
+
+    @wire(getSimilarBoats,{boatId: '$boatId', similarBy:'$similarBy'})
+    similarBoats({error, data}){
+        if(data){
+            this.relatedBoats = data;
+            this.error = undefined;
+        } else if(error){
+            this.error = error;
+        }
+    }
+
+    get getTitle(){
+        return 'Similar boats by ' + this.similarBy;
+    }
+
+    get noBoats(){
+        return !(this.relatedBoats && this.relatedBoats.length>0);
+    }
+
+    openBoatDetailPage(event) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.detail.boatId,
+                objectApiName: BOAT_OBJECT,
+                actionName: 'view'
+            },
+        });
+    }
+}
